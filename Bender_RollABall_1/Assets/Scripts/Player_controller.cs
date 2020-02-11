fe1535d9b5d40a7e3c5096e6f5ca331013e65d60
tannerbender <54556211+tannerbender@users.noreply.github.com>
2020-02-11 17:04:51 -0500

    updating roll a ball

--- Bender_RollABall_1/Assets/Scripts/Player_controller.cs
+++ Bender_RollABall_1/Assets/Scripts/Player_controller.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_controller : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     // Start is called before the first frame update
     void Start()
     {
+
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetCountText();
+
+        winText.text = "";
         
     }
 
@@ -25,5 +36,22 @@ public class Player_controller : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 11)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
