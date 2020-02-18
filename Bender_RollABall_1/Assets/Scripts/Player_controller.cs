ce7ffdc97363b45a359cdce1425160c4e0f8d83b
tannerbender <54556211+tannerbender@users.noreply.github.com>
2020-02-18 15:19:50 -0500

    adds cubes and scripts

--- Bender_RollABall_1/Assets/Scripts/Player_controller.cs
+++ Bender_RollABall_1/Assets/Scripts/Player_controller.cs
@@ -6,11 +6,11 @@ using UnityEngine.UI;
 public class Player_controller : MonoBehaviour
 {
     public float speed;
-    public Text countText;
-    public Text winText;
+   // public Text countText;
+   // public Text winText;
 
     private Rigidbody rb;
-    private int count;
+    //private int count;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +18,11 @@ public class Player_controller : MonoBehaviour
 
         rb = GetComponent<Rigidbody>();
 
-        count = 0;
+       // count = 0; 
 
-        SetCountText();
+       // SetCountText();
 
-        winText.text = "";
+       //   winText.text = "";
         
     }
 
@@ -39,19 +39,19 @@ public class Player_controller : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Pickup"))
+        if (other.gameObject.CompareTag("Pick up"))
         {
             other.gameObject.SetActive(false);
-            count = count + 1;
-            SetCountText();
+            //count = count + 1;
+           // SetCountText();
         }
     }
-    void SetCountText()
-    {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 11)
-        {
-            winText.text = "You Win!";
-        }
-    }
+   // void SetCountText()
+   // {
+        //countText.text = "Count: " + count.ToString();
+        //if (count >= 11)
+       // {
+          //  winText.text = "You Win!";
+       // }
+   // }
 }
