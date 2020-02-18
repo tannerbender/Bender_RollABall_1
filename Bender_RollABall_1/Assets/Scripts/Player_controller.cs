d22a9f8109c3eabbc0724bc684fffcaacb35f531
tannerbender <54556211+tannerbender@users.noreply.github.com>
2020-02-18 15:35:18 -0500

    adds count text and start of win text

--- Bender_RollABall_1/Assets/Scripts/Player_controller.cs
+++ Bender_RollABall_1/Assets/Scripts/Player_controller.cs
@@ -6,24 +6,22 @@ using UnityEngine.UI;
 public class Player_controller : MonoBehaviour
 {
     public float speed;
-   // public Text countText;
-   // public Text winText;
+    public Text countText;
+    // public Text winText;
 
     private Rigidbody rb;
-    //private int count;
+    private int count;
 
     // Start is called before the first frame update
     void Start()
     {
 
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
 
-       // count = 0; 
+        //   winText.text = "";
 
-       // SetCountText();
-
-       //   winText.text = "";
-        
     }
 
     // Update is called once per frame
@@ -32,7 +30,7 @@ public class Player_controller : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        
+
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
@@ -42,16 +40,14 @@ public class Player_controller : MonoBehaviour
         if (other.gameObject.CompareTag("Pick up"))
         {
             other.gameObject.SetActive(false);
-            //count = count + 1;
-           // SetCountText();
+            count = count + 1;
+            SetCountText();
+
         }
     }
-   // void SetCountText()
-   // {
-        //countText.text = "Count: " + count.ToString();
-        //if (count >= 11)
-       // {
-          //  winText.text = "You Win!";
-       // }
-   // }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
 }
+     
